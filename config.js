6bd15e8778e716b2b8d64c90e2245819b03ae0f7
Benjamin Lutz <benjamin.lutz@gmail.com>
2015-04-22 18:45:44 +0200

    added documentation to config.js file

--- config.js
+++ config.js
@@ -1,14 +1,51 @@
 var config = {};
 
+/**
+ * Defines the environment or stage in which the application runs.
+ *
+ * @type {string} dev or production.
+ */
 config.environment = 'dev';
 
+/**
+ * The port on which the server will listen.
+ *
+ * @type {number} as the easiest choice, take a port number > 1024.
+ */
 config.port = 3000;
 
+/**
+ * The secret which will be used for the hashing of the JSON Web Token.
+ *
+ * Please change it before you start the application for the first time!
+ *
+ * @type {string} the secret.
+ */
 config.jwtSecret = 'pL3aS3_ChAnG3_M3!';
+
+/**
+ * The expiry time of the JSON Web Token in minutes.
+ *
+ * @type {number} the expiry time in minutes.
+ */
 config.jwtExpiryTimeInMinutes = 600;
 
+/**
+ * The connection string of the mongoDB.
+ *
+ * See http://docs.mongodb.org/manual/reference/connection-string/ for further information.
+ *
+ * @type {string} the mongoDB connection string.
+ */
 config.mongoDB = 'mongodb://localhost/timaxjs';
 
+/**
+ * The configuration of the logger.
+ *
+ * See https://github.com/trentm/node-bunyan/blob/master/README.md for further information.
+ *
+ * @type {{name: string, streams: *[]}} logger config object.
+ */
 config.logger = {
     name: 'timax.js-rest',
     streams: [
